462f5160c1ef0403884548884907daa2c8da9f34
Sam ouhra <ouhrac@gmail.com>
2024-03-22 19:51:13 +0000

    update the manifest for the excluded matches

---
+++ chrome/src/firebase/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
+}
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
+const storage = getStorage(app)
+
+export { auth, db, storage }
